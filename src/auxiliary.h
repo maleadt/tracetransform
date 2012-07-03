2d0dc7335c274308a4852da2651d4e1500cf83a6
Tim Besard <tim.besard@elis.ugent.be>
2012-07-03 11:49:32 +0200

    Remove unused colormap and equalization routines.

--- src/auxiliary.h
+++ src/auxiliary.h
@@ -14,18 +14,6 @@
 // Data
 //
 
-const unsigned int palette_jet[] = {
-	0x000090,
-	0x000fff,
-	0x0090ff,
-	0x0fffee,
-	0x90ff70,
-	0xffee00,
-	0xff7000,
-	0xee0000,
-	0x7f0000
-};
-
 // Point clipping bitcodes
 typedef int ClipCode;
 const int INSIDE = 0;	// 0000
@@ -139,87 +127,6 @@ inline double deg2rad(double degrees)
 inline double rad2deg(double degrees)
 {
 	return (degrees * 180.0 / M_PI);
-}
-
-cv::Mat equalize(const cv::Mat &image)
-{
-	assert(image.type() == CV_8UC1);
-	cv::Mat equalized(image.size(), image.type());
-
-	// Determine the range
-	uchar min = 255, max = 0;
-	for (int i = 0; i < image.rows; i++) {
-		for (int j = 0; j < image.cols; j++) {
-			uchar pixel = image.at<uchar>(i, j);
-			if (min > pixel)
-				min = pixel;
-			if (pixel > max)
-				max = pixel;
-		}
-	}
-	if (min == 0 && max == 255)
-		return image;
-
-	// Equalize the range
-	uchar range = (uchar) (max - min);
-	double adaption = 255.0 / range;
-	for (int i = 0; i < image.rows; i++) {
-		for (int j = 0; j < image.cols; j++) {
-			uchar pixel = image.at<uchar>(i, j);
-			equalized.at<uchar>(i, j) = (uchar) ((pixel - min) * adaption);
-		}
-	}
-	return equalized;
-}
-
-cv::Mat colormap(const cv::Mat &image)
-{
-	assert(image.type() == CV_8UC1);
-	cv::Mat colormap(image.size(), image.type());
-	cvtColor(colormap, colormap, CV_GRAY2BGR);
-
-	// Equalize the range
-	cv::Mat equalized = equalize(image);
-
-	// Partition the colors
-	double partitioning = 255.0 / (sizeof(palette_jet)/sizeof(unsigned int) - 1);
-	for (int i = 0; i < equalized.rows; i++) {
-		for (int j = 0; j < equalized.cols; j++) {
-			uchar pixel = equalized.at<uchar>(i, j);
-
-			double c = pixel / partitioning;
-			double c_fract, c_int;
-			c_fract = std::modf(c, &c_int);
-
-			uchar r, g, b;
-			if (c_fract < std::numeric_limits<double>::epsilon()) {
-				r = (palette_jet[(int)c] >> 16) & 0xFF;
-				g = (palette_jet[(int)c] >> 8) & 0xFF;
-				b = palette_jet[(int)c] & 0xFF;
-			} else {
-				uchar r1, g1, b1;
-				r1 = (palette_jet[(int)c_int] >> 16) & 0xFF;
-				g1 = (palette_jet[(int)c_int] >> 8) & 0xFF;
-				b1 = palette_jet[(int)c_int] & 0xFF;
-
-				uchar r2, g2, b2;
-				r2 = (palette_jet[(int)c_int+1] >> 16) & 0xFF;
-				g2 = (palette_jet[(int)c_int+1] >> 8) & 0xFF;
-				b2 = palette_jet[(int)c_int+1] & 0xFF;
-
-				// Linear interpolation
-				r = (uchar) (r1*(1-c_fract) + r2*c_fract);
-				g = (uchar) (g1*(1-c_fract) + g2*c_fract);
-				b = (uchar) (b1*(1-c_fract) + b2*c_fract);
-			}
-
-			colormap.at<cv::Vec3b>(i, j)[0] = b;
-			colormap.at<cv::Vec3b>(i, j)[1] = g;
-			colormap.at<cv::Vec3b>(i, j)[2] = r;
-		}
-	}
-
-	return colormap;
 }
 
 // Clip a point against a rectangle
