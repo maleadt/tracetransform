859f73f307fc45329701751683078bfe2caeaef6
Tim Besard <tim.besard@elis.ugent.be>
2012-07-25 16:03:04 +0200

    Allow usage without specifying any P-functional.

--- src/main.cpp
+++ src/main.cpp
@@ -22,7 +22,7 @@
 #include "orthocircusfunction.h"
 
 // Debug flags
-//#define DEBUG_IMAGES
+#define DEBUG_IMAGES
 
 
 //
@@ -82,18 +82,18 @@ enum PFunctionalType {
 int main(int argc, char **argv)
 {
 	// Check and read the parameters
-	if (argc < 4) {
+	if (argc < 3) {
 		std::cerr << "Invalid usage: " << argv[0]
-			<< " INPUT T-FUNCTIONALS P-FUNCTIONALS" << std::endl;
+			<< " INPUT T-FUNCTIONALS [P-FUNCTIONALS]" << std::endl;
 		return 1;
 	}
 	std::string fn_input = argv[1];
 
 	// Get the chosen T-functionals
+	std::vector<TFunctional<double,double>*> tfunctionals;
 	std::stringstream ss;
 	ss << argv[2];
 	unsigned short i;
-	std::vector<TFunctional<double,double>*> tfunctionals;
 	while (!ss.eof()) {
 		ss >> i;
 		if (ss.fail()) {
@@ -130,48 +130,50 @@ int main(int argc, char **argv)
 	}
 
 	// Get the chosen P-functional
-	ss.clear();
-	ss << argv[3];
 	std::vector<PFunctional<double,double>*> pfunctionals;
-	while (!ss.eof()) {
-		PFunctionalType type = REGULAR;
-		if (ss.peek() == 'H') {
-			type = HERMITE;
-			ss.ignore();
-		}
+	if (argc >= 4) {
+		ss.clear();
+		ss << argv[3];
+		while (!ss.eof()) {
+			PFunctionalType type = REGULAR;
+			if (ss.peek() == 'H') {
+				type = HERMITE;
+				ss.ignore();
+			}
 
-		ss >> i;
-		if (ss.fail()) {
-			std::cerr << "Error: unparseable P-functional identifier" << std::endl;
-			return 1;
-		}
-
-		switch (type) {
-		case REGULAR:
-		{
-			switch (i) {
-			case 1:
-				pfunctionals.push_back(new PFunctional1<double>());
-				break;
-			case 2:
-				pfunctionals.push_back(new PFunctional2<double>());
-				break;
-			case 3:
-				pfunctionals.push_back(new PFunctional3<double>());
-				break;
-			default:
-				std::cerr << "Error: invalid P-functional provided" << std::endl;
+			ss >> i;
+			if (ss.fail()) {
+				std::cerr << "Error: unparseable P-functional identifier" << std::endl;
 				return 1;
 			}
-			break;
-		}
-		case HERMITE:
-			pfunctionals.push_back(new PFunctionalHermite<double>(i));
-			break;	
-		}
 
-		if (ss.peek() == ',')
-			ss.ignore();
+			switch (type) {
+			case REGULAR:
+			{
+				switch (i) {
+				case 1:
+					pfunctionals.push_back(new PFunctional1<double>());
+					break;
+				case 2:
+					pfunctionals.push_back(new PFunctional2<double>());
+					break;
+				case 3:
+					pfunctionals.push_back(new PFunctional3<double>());
+					break;
+				default:
+					std::cerr << "Error: invalid P-functional provided" << std::endl;
+					return 1;
+				}
+				break;
+			}
+			case HERMITE:
+				pfunctionals.push_back(new PFunctionalHermite<double>(i));
+				break;	
+			}
+
+			if (ss.peek() == ',')
+				ss.ignore();
+		}
 	}
 
 	// Read the image
@@ -252,40 +254,43 @@ int main(int argc, char **argv)
 	}
 	std::cerr << std::endl;
 
-	// Output the headers
-	decimals += 2;
-	std::cout << "#  ";
-	std::cout << std::setiosflags(std::ios::fixed) << std::setprecision(0);
-	for (size_t tp = 0; tp < (unsigned)data.rows; tp++) {
-		size_t t = tp / pfunctionals.size();
-		size_t p = tp % pfunctionals.size();
-		std::stringstream header;
-		header << t << ordinalSuffix(t) << " T, "
-			<< p << ordinalSuffix(p) << " P";
-		std::cout << std::setw(decimals) << header.str();
-	}
-	std::cout << "\n";
-
-	// Output the data
-	std::cout << std::setiosflags(std::ios::fixed) << std::setprecision(2);
-	for (int i = 0; i < data.cols; i++) {
-		std::cout << "   ";
-		for (int tp = 0; tp < data.rows; tp++) {
-			std::cout << std::setw(decimals)
-				<< data.at<double>(tp, i);
+	// Display the results of the P-functionals
+	if (pfunctionals.size() > 0) {
+		// Headers
+		decimals += 2;
+		std::cout << "#  ";
+		std::cout << std::setiosflags(std::ios::fixed) << std::setprecision(0);
+		for (size_t tp = 0; tp < (unsigned)data.rows; tp++) {
+			size_t t = tp / pfunctionals.size();
+			size_t p = tp % pfunctionals.size();
+			std::stringstream header;
+			header << t << ordinalSuffix(t) << " T, "
+				<< p << ordinalSuffix(p) << " P";
+			std::cout << std::setw(decimals) << header.str();
 		}
 		std::cout << "\n";
-	}
 
-	// Output the footer
-	std::cout << std::setiosflags(std::ios::fixed) << std::setprecision(0);
-	std::cout << "#  ";
-	for (size_t tp = 0; tp < (unsigned)data.rows; tp++) {
-		std::stringstream runtime;
-		runtime << 1000.0*runtimes[tp] << "ms";
-		std::cout << std::setw(decimals) << runtime.str();
+		// Data
+		std::cout << std::setiosflags(std::ios::fixed) << std::setprecision(2);
+		for (int i = 0; i < data.cols; i++) {
+			std::cout << "   ";
+			for (int tp = 0; tp < data.rows; tp++) {
+				std::cout << std::setw(decimals)
+					<< data.at<double>(tp, i);
+			}
+			std::cout << "\n";
+		}
+
+		// Footer
+		std::cout << std::setiosflags(std::ios::fixed) << std::setprecision(0);
+		std::cout << "#  ";
+		for (size_t tp = 0; tp < (unsigned)data.rows; tp++) {
+			std::stringstream runtime;
+			runtime << 1000.0*runtimes[tp] << "ms";
+			std::cout << std::setw(decimals) << runtime.str();
+		}
+		std::cout << std::endl;
 	}
-	std::cout << std::endl;
 
 	// Give the user time to look at the images
 	#ifdef DEBUG_IMAGES
