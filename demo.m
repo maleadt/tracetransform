e0d1f749f8f8dace7a2b272918c34763b5225818
Tim Besard <tim.besard@elis.ugent.be>
2012-07-30 13:26:45 +0200

    Displaying global execution time.

--- demo.m
+++ demo.m
@@ -1,13 +1,13 @@
 clear all;
 %% Demo of extraction of trace transform signatures 
 A = imread('../test/Cam1_V1.pgm','pgm');        % Reading Image
-A_smth = mat2gray(adapthisteq(A));      % Image pre-processing, illumination normalization.
+A_smth = mat2gray((A));      % Image pre-processing, illumination normalization.
 
 %% Extraction of feature vectors
 Code_Tfunct = [2];                      %Code of T functionals to be used, numbers comprised 
                                         %between (1 to 7), which correspond to the files "FunctionalT1.c" 
                                         
-Code_Pfunct = [4 5 6 7];                %Code of P functionals to be used, numbers between 1 and 3 
+Code_Pfunct = [4];                %Code of P functionals to be used, numbers between 1 and 3 
                                         %use the classical P-functionals defined in "Apply_Pfunct",
                                         % from 4 onwards the Hermite functionals are employed.
                                         
