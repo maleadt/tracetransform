5f2895b37d0747fc4af6be5026730dc8b482c36a
Tim Besard <tim.besard@elis.ugent.be>
2013-04-25 09:09:45 +0200

    Split off prefix sum.

--- src/kernels/functionals.cu
+++ src/kernels/functionals.cu
@@ -5,6 +5,9 @@
 // Header
 #include "functionals.hpp"
 
+// Standard library
+#include <stdio.h>
+
 // Local
 #include "../global.hpp"
 #include "../logger.hpp"
@@ -18,7 +21,28 @@ const int blocksize = 8;
 // Kernels
 //
 
-__global__ void findWeighedMedian_kernel(const float *_input,
+__global__ void prefixSum_kernel(const float *_input,
+                const int rows, const int cols,
+                float *_output)
+{
+        // Compute the thread dimensions
+        const int col = blockIdx.x * blockDim.x + threadIdx.x;
+        const int row = blockIdx.y * blockDim.y + threadIdx.y;
+
+        // Construct Eigen objects
+        Eigen::Map<const Eigen::MatrixXf> input(_input, rows, cols);
+        Eigen::Map<Eigen::MatrixXf> output(_output, rows, cols);
+
+        // Do we need to do stuff?
+        if (col < cols && row < rows) {
+                float sum = 0;
+                for (int row_above = 0; row_above <= row; row_above++)
+                        sum += input(row_above, col);
+                output(row, col) = sum;
+        }
+}
+
+__global__ void findWeighedMedian_kernel(const float *_input, const float *_prefix_sum,
                 const int rows, const int cols,
                 float *_output)
 {
@@ -27,18 +51,13 @@ __global__ void findWeighedMedian_kernel(const float *_input,
 
         // Construct Eigen objects
         Eigen::Map<const Eigen::MatrixXf> input(_input, rows, cols);
+        Eigen::Map<const Eigen::MatrixXf> prefix_sum(_prefix_sum, rows, cols);
         Eigen::Map<Eigen::VectorXf> output(_output, cols);
 
         // Do we need to do stuff?
         if (col < cols) {
-                float sum = 0;
-                for (int row = 0; row < rows; row++)
-                        sum += input(row, col);
-
-                float integral = 0;
                 for (int row = 0; row < rows; row++) {
-                        integral += input(row, col);
-                        if (2*integral >= sum) {
+                        if (2*prefix_sum(row, col) >= prefix_sum(rows-1, col)) {
                                 output(col) = row;
                                 break;
                         }
@@ -121,12 +140,21 @@ void TFunctional1(const CUDAHelper::GlobalMemory<float> *input, int rows,
         CUDAHelper::Chrono chrono;
         chrono.start();
 
+        // Launch prefix sum kernel
+        CUDAHelper::GlobalMemory<float> *prefix_sum = new CUDAHelper::GlobalMemory<float>(*input);
+        {
+                dim3 threads(blocksize, blocksize);
+                dim3 blocks(std::ceil((float)rows/blocksize), std::ceil((float)cols/blocksize));
+                prefixSum_kernel<<<blocks, threads>>>(*input, rows, cols, *prefix_sum);
+                CUDAHelper::checkState();
+        }
+
         // Launch weighed median kernel
         CUDAHelper::GlobalMemory<float> *medians = new CUDAHelper::GlobalMemory<float>(cols, 0);
         {
                 dim3 threads(blocksize, 1);
                 dim3 blocks(std::ceil((float)rows/blocksize), 1);
-                findWeighedMedian_kernel<<<blocks, threads>>>(*input, rows, cols, *medians);
+                findWeighedMedian_kernel<<<blocks, threads>>>(*input, *prefix_sum, rows, cols, *medians);
                 CUDAHelper::checkState();
         }
 
