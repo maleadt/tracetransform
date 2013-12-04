56da15ef385d5ea5a50cff9e34bd7bc6db8294bb
Tim Besard <tim.besard@elis.ugent.be>
2013-12-04 15:43:07 +0100

    Add rotation test script.

---
+++ rottest.jl
@@ -0,0 +1,45 @@
+#!/usr/bin/julia
+
+include("geometry.jl")
+include("auxiliary.jl")
+
+using Images
+
+function main(args::Vector{Any})
+    # Manage parameters
+    if length(args) != 2
+        error("Please provide input filename and rotation angle")
+    end
+    filename = args[1]
+    angle = uint(args[2])
+
+    # Read image
+    input::Image{Float64} = gray2mat(imread(filename))
+    printmat(int(255*input))
+
+    # Rotate image
+    origin::Vector{Float64} = floor(([size(input)...] .+ 1) ./ 2)
+    input_rotated::Image{Float64} = rotate(input, origin, angle)
+
+    # Output image
+    print("\n")
+    output = mat2gray(input_rotated)
+    printmat(output)
+end
+
+function printmat(input)
+    for row in 1:size(input, "y")
+        for col in 1:size(input, "x")
+            pixel = input["y", row, "x", col]
+            if pixel == 0
+                decimals = 1
+            else
+                decimals = ifloor(log10(pixel))+1
+            end
+            print("$pixel$(" "^(4-decimals))")
+        end
+        print("\n")
+    end
+end
+
+main(ARGS)
