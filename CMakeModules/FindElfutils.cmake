fb5fadb733880d67d4e54a181742f1cc88cf1c5a
Tim Besard <tim.besard@elis.ugent.be>
2013-12-13 15:29:58 +0100

    Improve elfutils detection.
    Conflicts:
    	CMakeLists.txt
    	CMakeModules/FindElfutils.cmake

--- CMakeModules/FindElfutils.cmake
+++ CMakeModules/FindElfutils.cmake
@@ -1,13 +1,27 @@
-FIND_PATH(Elfutils_ROOT_DIR elfutils/libdw.h
-	$ENV{Elfutils_ROOT_DIR})
+# - Find elfutils
+# Find elfutils libraries
+#
+# This module defines the following variables:#
+#   ELFUTILS_FOUND        : true if everything is found
+#   ELFUTILS_INCLUDE_DIR  : where to find dw.h, etc.
+#   ELFUTILS_LIBRARIES    : the library to link against.
 
-SET(Elfutils_INCLUDE_DIR ${Elfutils_ROOT_DIR})
+FIND_PATH(ELFUTILS_INCLUDE_DIR elfutils/version.h
+    $ENV{Elfutils_ROOT_DIR})
 
-FIND_LIBRARY(DW_LIBRARY 
+
+# DWARF library
+
+FIND_LIBRARY(DWARF_LIBRARY 
   NAMES dw 
   PATHS /usr/lib
         /usr/$ENV{MACHTYPE}/lib)
 
-IF (Elfutils_INCLUDE_DIR)
-        SET(Elfutils_FOUND TRUE)
-ENDIF (Elfutils_INCLUDE_DIR)
+# Finish up
+
+SET(ELFUTILS_LIBRARIES DWARF_LIBRARY)
+
+MARK_AS_ADVANCED(ELFUTILS_INCLUDE_DIR ELFUTILS_LIBRARIES)
+
+INCLUDE(FindPackageHandleStandardArgs)
+FIND_PACKAGE_HANDLE_STANDARD_ARGS(Elfutils DEFAULT_MSG ELFUTILS_INCLUDE_DIR ELFUTILS_LIBRARIES)
