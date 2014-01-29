9f3e29678f878b4ccfd74894c81f313bd9eab0fe
Tim Besard <tim.besard@elis.ugent.be>
2014-01-29 09:32:36 +0100

    Clean-up bitonic sort, add shared memory, allow for non-pow2 array lengths.

--- src/kernels/functionals.cu
+++ src/kernels/functionals.cu
@@ -8,6 +8,9 @@
 // Standard library
 #include <stdio.h>
 
+// CUDA
+#include <math_constants.h>
+
 // Local
 #include "../logger.hpp"
 
@@ -586,53 +589,64 @@ __global__ void transformMedianDomain_kernel(const float *input,
         output[row + col * rows] = 0;
 }
 
-// TODO: support non-pow2 row lengths
-__device__ void sortBitonicStep(float *input, unsigned int major,
-                                       unsigned int minor) {
+__device__ inline void swap(float &a, float &b) {
+    // Alternative swap doesn't use a temporary register:
+    // a ^= b;
+    // b ^= a;
+    // a ^= b;
+
+    float tmp = a;
+    a = b;
+    b = tmp;
+}
+
+// TODO: properly support non-pow2 row lengths
+//       http://www.iti.fh-flensburg.de/lang/algorithmen/sortieren/bitonic/oddn.htm
+__global__ void sortBitonic_kernel(float *input, const int rows_input) {
+    // Shared memory
+    extern __shared__ float temp[];
+
     // Compute the thread dimensions
     const int col = blockIdx.x;
     const int row = threadIdx.y;
     const int rows = blockDim.y;
 
-    // Find sparring partner
-    unsigned int row2 = row ^ minor;
+    // Fetch
+    if (row < rows_input)
+        temp[row] = input[row + col * rows_input];
+    else
+        temp[row] = CUDART_INF_F;
+    __syncthreads();
 
-    // The threads with the lowest ids sort the array
-    if (row2 > row) {
+    // Sort
+    for (unsigned int major = 2; major <= rows; major *= 2) {
+        // Merge
+        for (unsigned int minor = major / 2; minor > 0; minor /= 2) {
+            // Find sorting partner
+            unsigned int row2 = row ^ minor;
+
+            // The threads processing the lowest row sort the array
+            if (row2 > row) {
         if ((row & major) == 0) {
-            // Ascending sort
-            if (input[row + col * rows] > input[row2 + col * rows]) {
-                // swap(row, row2)
-                float temp = input[row + col * rows];
-                input[row + col * rows] = input[row2 + col * rows];
-                input[row2 + col * rows] = temp;
+            // Sort ascending
+            if (temp[row] > temp[row2]) {
+                swap(temp[row], temp[row2]);
+            }
+        } else {
+            // Sort descending
+            if (temp[row] < temp[row2]) {
+                swap(temp[row], temp[row2]);
             }
         }
-        if ((row & major) != 0) {
-            // Descending sort
-            if (input[row + col * rows] < input[row2 + col * rows]) {
-                // swap(row, row2)
-                float temp = input[row + col * rows];
-                input[row + col * rows] = input[row2 + col * rows];
-                input[row2 + col * rows] = temp;
             }
-        }
-    }
-}
 
-__global__ void sortBitonic_kernel(float *input) {
-    // Compute the thread dimensions
-    const int rows = blockDim.y;
-
-    for (unsigned int major = 2; major <= rows;
-         major <<= 1) {
-        for (unsigned int minor = major >> 1; minor > 0;
-             minor = minor >> 1) {
-            sortBitonicStep(input, major, minor);
             __syncthreads();
-        }
+    }
     }
 
+    // Write back
+    if (row < rows_input)
+        input[row + col * rows_input] = temp[row];
 }
 
 // NOTE: this is just the weighted median kernel + value writeback
